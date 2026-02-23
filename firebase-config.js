6ee9644da1f898fda3a959088ca8f8a79373210b
bilalsirwebdeveloper-a11y <bilalsirwebdeveloper@gmail.com>
2026-02-23 07:53:45 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
-// Your Firebase configuration
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1C1mHbmGaBxfdsQ34Cosgoned1Cg5cWQ",
-  authDomain: "groupmela.firebaseapp.com",
-  databaseURL: "https://groupmela-default-rtdb.firebaseio.com",
-  projectId: "groupmela",
-  storageBucket: "groupmela.firebasestorage.app",
-  messagingSenderId: "608978556223",
-  appId: "1:608978556223:web:905c071ee45c2b010d6f8a",
-  measurementId: "G-CPTNDSGTYE"
+    apiKey: "AIzaSyD1C1mHbmGaBxfdsQ34Cosgoned1Cg5cWQ",
+    authDomain: "groupmela.firebaseapp.com",
+    databaseURL: "https://groupmela-default-rtdb.firebaseio.com",
+    projectId: "groupmela",
+    storageBucket: "groupmela.firebasestorage.app",
+    messagingSenderId: "608978556223",
+    appId: "1:608978556223:web:905c071ee45c2b010d6f8a",
+    measurementId: "G-CPTNDSGTYE"
 };
 
 // Initialize Firebase
@@ -15,3 +15,5 @@ firebase.initializeApp(firebaseConfig);
 
 // Get database reference
 const database = firebase.database();
+
+console.log('🔥 Firebase initialized successfully!');
